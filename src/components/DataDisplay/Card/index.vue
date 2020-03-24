2d97fbc517cea7008b9a6cf68d60bfa1219b7326
JP Casabianca <hola@jpcasabianca.com>
2020-03-24 15:35:36 -0500

    Updated linting

--- src/components/DataDisplay/Card/index.vue
+++ src/components/DataDisplay/Card/index.vue
@@ -9,15 +9,15 @@ export default {
   computed: {
     cardStyles() {
       return {
-        backgroundColor: '#fff',
-        borderRadius: '0.5rem',
-        padding: '1rem',
+        backgroundColor: "#fff",
+        borderRadius: "0.5rem",
+        padding: "1rem",
         boxShadow:
-          '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)'
-      }
+          "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)"
+      };
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped></style>
