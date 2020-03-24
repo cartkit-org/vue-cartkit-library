72ed01b77b5c33d81e05170cd6ddce4de047af85
JP Casabianca <hola@jpcasabianca.com>
2020-03-24 15:33:30 -0500

    Added Card component

---
+++ src/components/DataDisplay/Card/index.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <div class="card" :style="cardStyles">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    cardStyles() {
+      return {
+        backgroundColor: '#fff',
+        borderRadius: '0.5rem',
+        padding: '1rem',
+        boxShadow:
+          '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import Button from "./components/General/Button";
 import Loader from "./components/General/Loader";
 import Divider from "./components/Layout/Divider";
 import EmptyState from "./components/Layout/EmptyState";
+import Card from "./components/DataDisplay/Card";
 
 // Export components individually
-export { SalesPop, Button, Loader, Divider, EmptyState };
+export { SalesPop, Button, Loader, Divider, EmptyState, Card };
 
 // What should happen if the user installs the library as a plugin
 function install(Vue) {
@@ -14,6 +15,7 @@ function install(Vue) {
   Vue.component("cartkit-loader", Loader);
   Vue.component("cartkit-divider", Divider);
   Vue.component("cartkit-empty-state", EmptyState);
+  Vue.component("cartkit-card", Card);
 }
 
 // Export the library as a plugin
